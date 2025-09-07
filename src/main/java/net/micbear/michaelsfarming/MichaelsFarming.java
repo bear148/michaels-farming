bfd07f97077a27ec816ef3f9ad261d3e9e77dd39
bear148 <bizzeebear@hotmail.com>
2025-09-06 18:25:57 -0700

    Initial commit

---
+++ src/main/java/net/micbear/michaelsfarming/MichaelsFarming.java
@@ -0,0 +1,16 @@
+package net.micbear.michaelsfarming;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MichaelsFarming implements ModInitializer {
+	public static final String MOD_ID = "michaelsfarming";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/micbear/michaelsfarming/MichaelsFarmingClient.java
@@ -0,0 +1,10 @@
+package net.micbear.michaelsfarming;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MichaelsFarmingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
