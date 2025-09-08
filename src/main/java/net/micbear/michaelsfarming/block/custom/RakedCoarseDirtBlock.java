5a3457f200d2089a4ade723ece460a87806e0829
bear148 <bizzeebear@hotmail.com>
2025-09-08 11:26:05 -0700

    Reworking raked coarse dirt

---
+++ src/main/java/net/micbear/michaelsfarming/block/custom/RakedCoarseDirtBlock.java
@@ -0,0 +1,9 @@
+package net.micbear.michaelsfarming.block.custom;
+
+import net.minecraft.block.FarmlandBlock;
+
+public class RakedCoarseDirtBlock extends FarmlandBlock {
+    public RakedCoarseDirtBlock(Settings settings) {
+        super(settings);
+    }
+}
