1eac3178d71b993bb103bcfe1359596fd7657363
bear148 <bizzeebear@hotmail.com>
2025-09-09 10:58:15 -0700

    Finished raked coarse dirt rework. Modified the model to more closely match vanilla farmland.

--- src/main/java/net/micbear/michaelsfarming/block/ModBlocks.java
+++ src/main/java/net/micbear/michaelsfarming/block/ModBlocks.java
@@ -1,16 +1,20 @@
 package net.micbear.michaelsfarming.block;
 
 import net.micbear.michaelsfarming.MichaelsFarming;
+import net.micbear.michaelsfarming.block.custom.RakedCoarseDirtBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import java.util.function.Function;
 
 /*
 * This class doesn't really have any use as of right now. I want to focus on adding farming stuffs to the game before
@@ -19,18 +23,24 @@ import net.minecraft.util.Identifier;
 
 public class ModBlocks {
     public static final Block CROP_MERCHANT_BLOCK = registerBlock("crop_merchant_block",
+            Block::new,
             AbstractBlock.Settings.create()
                     .strength(4f)
                     .sounds(BlockSoundGroup.WOOD));
+
     public static final Block RAKED_COARSE_DIRT = registerBlock("raked_coarse_dirt",
-            AbstractBlock.Settings.create().strength(4f).sounds(BlockSoundGroup.ROOTED_DIRT));
+            RakedCoarseDirtBlock::new,
+            AbstractBlock.Settings.create()
+                    .strength(0.6f)
+                    .sounds(BlockSoundGroup.GRAVEL));
 
-    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
-        RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(MichaelsFarming.MOD_ID, name));
-        Block block = new Block(blockSettings.registryKey(key));
-        registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, key, block);
 
+    private static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
+        Identifier id = Identifier.of(MichaelsFarming.MOD_ID, name);
+        RegistryKey<Block> blockKey = RegistryKey.of(RegistryKeys.BLOCK, id);
+        Block block = Blocks.register(blockKey, factory, settings);
+        Items.register(block);
+        return block;
     }
 
     private static void registerBlockItem(String name, Block block) {
